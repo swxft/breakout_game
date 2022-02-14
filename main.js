b9a8c87f2db8046bb268d352433c071a243f07c6
swxft <edenweb@outlook.com>
2022-02-14 10:39:28 -0500

    paddle, boundaries, game over handling added

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth) / 2;
+let isRightPressed = false;
+let isLeftPressed = false;
 
+// Utilities
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -14,25 +20,74 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Movement, Boundary, & Game-Over Handling
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-
-    x += dx;
-    y += dy;
+    drawPaddle();
     
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
 
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
     }
 
+    if(isRightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(isLeftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
     x += dx;
     y += dy;
 }
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-setInterval(draw, 10);
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        isRightPressed = true;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        isLeftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        isRightPressed = false;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        isLeftPressed = false;
+    }
+}
+
+
+const interval = setInterval(draw, 10);
 
