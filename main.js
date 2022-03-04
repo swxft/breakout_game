cb3915a8af902c1148680b41e710da8850489c7f
swxft <edenweb@outlook.com>
2022-03-04 13:06:32 -0500

    upgrade .5 checkpoint

--- main.js
+++ main.js
@@ -20,62 +20,62 @@ const brickOffsetLeft = 30;
 let score = 0;
 let lives = 3;
 
-// Utilities
+
 const bricks = [];
-for(var c=0; c<brickColumnCount; c++) {
-    bricks[c] = [];
-    for(var r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0, status: 1 };
-    }
+for (var c = 0; c < brickColumnCount; c += 1) {
+  bricks[c] = [];
+  for (var r = 0; r < brickRowCount; r += 1) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
 }
 
 function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
 }
 
 function drawLives() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+  ctx.font = '16px Arial';
+  ctx.fillStyle = '#0095DD';
+  ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
 }
 
 function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
 }
 
 function drawBricks() {
-    for(var c=0; c<brickColumnCount; c++) {
-        for(var r=0; r<brickRowCount; r++) {
-            if(bricks[c][r].status == 1) {
-                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
-                ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#0095DD";
-                ctx.fill();
-                ctx.closePath();
-            }
-        }
+  for (let c = 0; c < brickColumnCount; c += 1) {
+    for (let r = 0; r < brickRowCount; r += 1) {
+      if (bricks[c][r].status === 1) {
+        const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
+      }
     }
+  }
 }
 
-let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+// const brickX = (c *(brickWidth + brickPadding)) + brickOffsetLeft;
+// const brickY = (r *(brickHeight + brickPadding)) + brickOffsetTop;
 
 function drawScore() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: "+score, 8, 20);
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#0095DD";
+	ctx.fillText("Score: "+score, 8, 20);
 }
 
 // Movement, Boundary, & Game-Over Handling
@@ -170,13 +170,12 @@ function mouseMoveHandler(e) {
 }
 
 function keyUpHandler(e) {
-    if (e.key == "Right" || e.key == "ArrowRight") {
-        isRightPressed = false;
-    }
-    else if (e.key == "Left" || e.key == "ArrowLeft") {
-        isLeftPressed = false;
-    }
+	if (e.key == "Right" || e.key == "ArrowRight") {
+			isRightPressed = false;
+	}
+	else if (e.key == "Left" || e.key == "ArrowLeft") {
+			isLeftPressed = false;
+	}
 }
 
 draw();
-
